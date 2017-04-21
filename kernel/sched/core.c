fdd9ec409e10da46d8169959d1f5282135d79ad5
Peter Zijlstra <peterz@infradead.org>
2017-04-21 12:46:57 +0200

    sched/clock: Use late_initcall() instead of sched_init_smp()
    Core2 marks its TSC unstable in ACPI Processor Idle, which is probed
    after sched_init_smp(). Luckily it appears both acpi_processor and
    intel_idle (which has a similar check) are mandatory built-in.
    
    This means we can delay switching to stable until after these drivers
    have ran (if they were modules, this would be impossible).
    
    Delay the stable switch to late_initcall() to allow these drivers to
    mark TSC unstable and avoid difficult stable->unstable transitions.
    
    Reported-by: Lofstedt, Marta <marta.lofstedt@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J . Wysocki <rafael.j.wysocki@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6121,7 +6121,6 @@ void __init sched_init_smp(void)
 	init_sched_dl_class();
 
 	sched_init_smt();
-	sched_clock_init_late();
 
 	sched_smp_initialized = true;
 }
@@ -6137,7 +6136,6 @@ early_initcall(migration_init);
 void __init sched_init_smp(void)
 {
 	sched_init_granularity();
-	sched_clock_init_late();
 }
 #endif /* CONFIG_SMP */
 
